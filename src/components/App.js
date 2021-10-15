830b2a84eec2d3203c8cc66131e3b555c0436c9e
kyleotoole91 <49255786+kyleotoole91@users.noreply.github.com>
2021-10-15 01:34:52 +0100

    Client now obtains Token smart contract

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react'
 import './App.css'
 import Navbar from './Navbar'
+import Token from '../abis/Token.json'
+//import FreedomFinance from '../abis/FreedomFinance.json'
 const Web3 = require('web3')
 
 const noWeb3Msg = 'Please install a Web3 provider (MetaMask)';
@@ -22,21 +24,6 @@ class App extends Component {
     await this.loadWeb3Data()
   }
 
-  async loadWeb3Data() {
-    const web3 = window.web3
-    const accounts = await web3.eth.getAccounts()
-    const accountBalance = await web3.eth.getBalance(accounts[0])
-    const etherBalance = web3.utils.fromWei(accountBalance, 'ether')
-
-    this.setState({ account: accounts[0],
-                    etherBalance: accountBalance})
-    console.log(this.state.account)
-
-    this.setState({ accountBalance,
-                    etherBalance })
-    console.log(this.state.accountBalance)
-  }
-
   async loadWeb3() {
     if (window.ethereum) {
       await window.ethereum
@@ -56,6 +43,30 @@ class App extends Component {
     }
   }
 
+  async loadWeb3Data() {
+    const web3 = window.web3
+    const accounts = await web3.eth.getAccounts()
+    const accountBalance = await web3.eth.getBalance(accounts[0])
+    const etherBalance = web3.utils.fromWei(accountBalance, 'ether')
+
+    this.setState({ account: accounts[0],
+                    etherBalance: accountBalance})
+    console.log(this.state.account)
+
+    this.setState({ accountBalance,
+                    etherBalance })
+    console.log(this.state.accountBalance)
+    
+    const networkId = await web3.eth.net.getId()
+    const tokenData = Token.networks[networkId]
+    if (tokenData) {
+      const token = new web3.eth.Contract(Token.abi, Token.networks[networkId].address)
+      console.log(Token)
+    } else {
+      window.alert('Unable to obtain Token smart contract from network')
+    }
+  }
+
   render() {
     return (
       <div>
@@ -65,7 +76,7 @@ class App extends Component {
             <main role="main" className="col-lg-12 d-flex text-center">
               <div className="content mr-auto ml-auto">
                 <h1>Welcome to Freedom Finance!</h1>
-                <h1>You Ether balance is : {this.state.etherBalance}</h1>
+                <h3>Your Ether balance is : {this.state.etherBalance}</h3>
               </div>
             </main>
           </div>
